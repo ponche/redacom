2d7df4c482a585f36df44762fec5ceb8030dc747
Rémi <ponche62880@gmail.com>
2018-11-11 17:06:43 +0100

    add content PV and the law in 6 question

--- services.php
+++ services.php
@@ -38,12 +38,57 @@
             </article>
             <article>
                 <h2>Compte rendu</h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda sapiente repudiandae commodi hic expedita quam odit officiis dolore dignissimos at! Quos esse doloremque molestias, obcaecati corrupti facere. Possimus, consequuntur quod.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor suscipit similique eum ducimus unde maxime molestiae optio laboriosam quia reiciendis. Non ad cumque quo reiciendis, distinctio ipsa accusantium beatae voluptas?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum laborum praesentium explicabo harum rerum enim, commodi recusandae eligendi cum voluptates quos accusantium blanditiis corrupti dolor laudantium ex et porro tenetur?
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ex illum placeat corrupti ipsam, consectetur praesentium excepturi dignissimos reiciendis eius rerum? Ad modi amet necessitatibus laudantium quisquam neque accusamus quos?
-                </p>
+                <h4> PV et la loi en 6 questions : </h4>
+                <div class=border-red>
+                <p>Les Procès-verbaux d’entreprise sont des documents incontournables. L’objectif de ces documents est de consigner les délibérations des CSE dans un
+document unique. Validé en début de chaque réunion, il est l’outil indispensable au bon déroulement des réunions. Il permet à tous de prendre
+connaissance des positions de chacun lors de la précédente réunion et de faire avancer le dialogue social.</p>
+<p>
+La rédaction de PV est un travail fastidieux qui demande beaucoup de temps aux Secrétaires de CSE. Afin que ces derniers puissent se concentrer sur
+d’autres tâches à plus forte valeur ajoutée, la loi permet aux secrétaires de se faire assister pour la rédaction de ces documents.</p>
+</div>
+
+
+<div><span class="question">1/ A qui incombe la responsabilité de la rédaction des procès-verbaux de CSE ?</span>
+
+<p class="answer">L'élaboration des procès-verbaux relève de la compétence du secrétaire de CSE. Il est à noter que la rédaction des PV est une obligation légale. </p>
+</div>
+
+
+<div><span class="question">2/ Le secrétaire peut-il se faire assister pour l’élaboration des PV ?</span>
+
+<p class="answer">La loi autorise les secrétaires de CSE à recourir à l'aide d'un professionnel pour la rédaction des procès-verbaux de réunion. </p>
+</div>
+
+
+<div>><span class="question">3/ Est-il possible d’enregistrer les débats ?</span>
+
+<p class="answer">Le CSE peut enregistrer les réunions, il doit cependant procéder à un vote. L'employeur ne peut pas s'opposer à l'enregistrement des réunions sauf si les informations communiquées sont confidentielles ou relèvent du secret professionnel.</p>
+</div>
+
+
+<div ><span class="question">4/ Dans quels délais les PV doivent-ils être rédigés ?</span>
+
+<p class="question">Les PV doivent être rédigés dans un délai de 15 jours maximum ou, le cas échéant, dans le délai prévu par l'accord d'entreprise.
+Des délais particuliers s'appliquent dans certaines circonstances :
+3 jours dans le cadre d'un projet de licenciement collectif pour motif économique,
+1 jour si la réunion concerne le redressement ou la liquidation judiciaire de l'entreprise.
+</p>
+</div>
+
+
+<div><span class="question">5/ Comment se déroule la validation des procès-verbaux ?</span>
+
+<p class="answer">Les procès-verbaux sont généralement approuvés lors de la réunion de CSE suivante. La validation du PV est généralement le premier point de l'ordre du jour. Lors de la validation de celui-ci, le président du CSE fait part de ses remarques avant que les élus ne passent au vote.</p>
+</div>
+
+
+<div><span class="question">6/ Les procès-verbaux sont-ils diffusés ? </span>
+
+<p class="answer">
+    Les PV peuvent être affichés ou diffusés sur l'intranet après leur adoption. Les diffusions externes de ces documents sont interdites sauf cas particuliers (transmission à l'expert-comptable, à l'inspection du travail...)
+</p>
+</div>
             </article>
         </section>
     </main>
