4d060ff8c5db7e204c6d297e01f82d385cacc223
Rémi <ponche62880@gmail.com>
2018-12-20 01:27:49 +0100

    modif 7 bonne raison passe en dessous sur mobile

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,9 +9,10 @@
     <!-- <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />  -->
     <link rel="stylesheet" type="text/css" media="screen" href="css/design.css" />
 </head>
+
 <body>
     <?php include("includes/headerNav.html") ; ?>
-    <main>
+    <main class="accueil">
         <aside>
             <q>7 bonnes raisons de nous confier la rédaction de vos procès-verbaux</q>
             <ol>
@@ -23,9 +25,21 @@
                 <li>Des délais de remise des PV adaptables à vos besoins</li>
             </ol>
         </aside>
+        <section class="mobile">
+            <q>7 bonnes raisons de nous confier la rédaction de vos procès-verbaux</q>
+            <ol>
+                <li>Un gain de temps assuré</li>
+                <li>Une qualité professionnelle</li>
+                <li>Un accompagnement personnalisé</li>
+                <li>Un Interlocteur unique</li>
+                <li>Un budget maîtrisé et ajusté</li>
+                <li>Une confidentialité garantie</li>
+                <li>Des délais de remise des PV adaptables à vos besoins</li>
+            </ol>
+        </section>
         <section class="section-accueil">
-             <!-- <img class="photoCarousel" src="images/bureau-with-logo-cahier.png" alt="bureau presentation" />  -->
-             <div>
+            <!-- <img class="photoCarousel" src="images/bureau-with-logo-cahier.png" alt="bureau presentation" />  -->
+            <div>
                 <span>CSE et organismes assimilés</span>
             </div>
             <div>
@@ -33,10 +47,11 @@
             </div>
             <div>
                 <span>Particuliers</span>
-            </div>  
+            </div>
 
         </section>
     </main>
     <!-- <script src="bootstrap/js/bootstrap.min.js"></script> -->
 </body>
+
 </html>
