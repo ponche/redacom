ee6175c2cc086929bb882c657b47d9358c098a11
Rémi <ponche62880@gmail.com>
2019-08-17 15:51:19 +0200

    add styles bootstrap in project and container for mainSection

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,7 @@ class DefaultController extends AbstractController
     
     public function index()
     {
-        return $this->render('base.html.twig'); 
+        return $this->render('main/home.html.twig'); 
     }
 
     /**
